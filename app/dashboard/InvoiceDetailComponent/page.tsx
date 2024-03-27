2d2984faea7472e8f59585f871f17a5abd321143
Roshan Kumar <rkroshan115@gmail.com>
2024-03-27 19:40:00 +0530

    first commit
    gchggh

---
+++ app/dashboard/InvoiceDetailComponent/page.tsx
@@ -0,0 +1,19 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import InvoicesTable from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <CreateInvoice />
+      </div>
+    </main>
+  );
+}
